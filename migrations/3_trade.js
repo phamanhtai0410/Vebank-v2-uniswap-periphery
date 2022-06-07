3c88bfc09ce5db5b5f16ea1e6f07234af8e3d2c9
TramVM <tramvm@gmail.com>
2022-06-07 13:56:45 +0700

    Can add liq and swap like DEX

--- migrations/3_trade.js
+++ migrations/3_trade.js
@@ -6,6 +6,8 @@ var IERC20 = artifacts.require("IERC20");
 const factory = "0x8fd2cEd4f6751FBB29186196c255C667f6636A25"; // From core
 const feeToSetter = "0x24d39F8c3c38Df135Fd28078e6A2e2dCF9284FBB";
 const rewardAddress = "0x24d39F8c3c38Df135Fd28078e6A2e2dCF9284FBB";
+const walletAddress = "0x24d39F8c3c38Df135Fd28078e6A2e2dCF9284FBB";
+const walletAddressReward = "";
 const WETH = "0x0000000000000000000000000000456e65726779";
 const tokenAAddress = "0xe88c871CEA576DdD59FA91a744Eb6C6d5b93AB40";
 const tokenBAddress = "0x0000000000000000000000000000456e65726779";
@@ -36,9 +38,18 @@ module.exports = async function (deployer) {
   await tokenA.approve(iUniswapV2Router02.address, "10000000000000000000000");
   await tokenB.approve(iUniswapV2Router02.address, "10000000000000000000000");
 
+  console.log("Balance before swap");
+  console.log("Pool token: A:", (await tokenA.balanceOf("0xd87b572e9559a62b015873be97f8ee3a10e05f60")).toString(), " B:", (await tokenB.balanceOf("0xd87b572e9559a62b015873be97f8ee3a10e05f60")).toString());
+  console.log("User Token A", (await tokenA.balanceOf(walletAddress)).toString());
+  console.log("User Token B", (await tokenB.balanceOf(walletAddress)).toString());
+
   // Add pool
-  await iUniswapV2Router02.addLiquidity( tokenA.address, tokenB.address, "10000000000000000000000",  "1000000000000000000000", 
-  "10000000000000000000000",  "1000000000000000000000", rewardAddress, 1657176936)
+  await iUniswapV2Router02.swapExactTokensForTokens("15000000000000000000", "250000000000000000", [tokenAAddress, tokenBAddress],  walletAddress, 1657176936)
+
+  console.log("Balance after swap");
+  console.log("Pool token: A:", (await tokenA.balanceOf("0xd87b572e9559a62b015873be97f8ee3a10e05f60")).toString(), " B:", (await tokenB.balanceOf("0xd87b572e9559a62b015873be97f8ee3a10e05f60")).toString());
+  console.log("Token A", (await tokenA.balanceOf(walletAddress)).toString());
+  console.log("Token B", (await tokenB.balanceOf(walletAddress)).toString());
 
   console.log("Done!!")
 };
